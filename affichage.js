a0bd8d24135afec7750c0f2d14444100f1205491
Arnold YAYI <87656513+arnold2905@users.noreply.github.com>
2021-09-28 16:43:47 +0200

    cours suite
    suite

---
+++ affichage.js
@@ -0,0 +1,27 @@
+//affichage js
+
+//let ville = 'Nice';
+//console.log(ville);
+
+let prixLogement = 1200;
+console.log(prixLogement);
+
+
+alert(prixLogement);
+alert('Bienvenue chez les ch\'tis !');
+
+//mini-exo : depmandez à un utilisateur sa ville de naissance
+
+
+//let villeNaissance = prompt('Quelle est votre ville de naissance?');
+//console.log(villeNaissance);
+
+//alert('Paris');
+
+prompt('Votre age?');
+
+let villeNaissance;
+
+villeNaissance = prompt('Bonjour, ou êtes-vous né ?');
+
+alert('Vous êtes né à ! + Paris');
---
+++ introduction.js
@@ -0,0 +1,54 @@
+//Introduction Javascript : Mon second fichier js
+
+//Création de  variables qui ne stockent pas de valeurs 
+
+let prenom;
+console.log(prenom);
+//let monAge;
+
+var ville;
+
+//le signe = est un opérateur d'affectation 
+prenom = 'Jean'; // cette variable contient une chaîne de caractères
+
+console.log(prenom);
+
+let monAge;
+monAge = 40; //cette variable contient un nombre  
+
+// ceci fonctionne avec une variable ; on initialise la viable après affectation
+nom = 'Yourcenar'
+var nom;
+console.log(nom);
+
+//ceci ne fonctionne pas ; avec let il faut initialiser la variable aveant affectation
+// vehicule = 'autobus'
+//let vehicule;
+//console.log(vehicule)
+
+var chien = 'Medor'
+console.log(chien);
+var chien = 'Kiki'
+console.log(chien);
+
+// ceci ne fonctionne pas 
+//let chat = 'Minet'
+//console.log(chat)
+//let chat = 'Minouchette';
+//let chat = 'Minouchette'
+//console.log(chat)
+
+
+if(true){
+    let animal = 'Tigre'
+    console.log(animal);
+}
+
+//console.log(animal);
+
+if(true){
+    var objet = 'Lampe'
+    console.log(objet)
+}
+
+console.log(objet)
