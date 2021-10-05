b8ee91010e783f147f60c1df147fc7f62e816319
Arnold YAYI <87656513+arnold2905@users.noreply.github.com>
2021-10-05 16:55:01 +0200

    suitecours
    suite

--- 03_structures/if.js
+++ 03_structures/if.js
@@ -2,11 +2,24 @@
 
 let x = 4;
 let y = 0;
-
+//1er
 if(x > 1) {
     alert('x contient ' + x + ' une valeur supérieure à 1');
 }
 
-if(x < 1) {
+//2ième
+if(y < 1) {
     alert('y contient ' + y + ' une valeur inférieure à 1');
 }
+
+//3ième
+if( x==y ) {
+    alert('3/ x = ' + x + ' et, y = ' + y + 'les deux variables contiennent la même valeur');
+}
+
+//4e
+
+if(y) {
+    alert('4/ la valeur de y est évaluée à "true"');
+}
+
