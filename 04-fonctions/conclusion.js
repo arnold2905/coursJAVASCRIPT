0923f26190e7b6010a59bd5c6c1f24b7647be00d
Arnold YAYI <87656513+arnold2905@users.noreply.github.com>
2021-10-15 15:58:14 +0200

    suite cours
    cours

--- 04-fonctions/conclusion.js
+++ 04-fonctions/conclusion.js
@@ -31,3 +31,24 @@ console.log(typeof para1,para1);
 para1.addEventListener('click', function () {
     alert('Tu as bien cliqué sur le paragraphe 1!');
 });
+
+
+let para2 = document.getElementById('p2');
+console.log(typeof para2,para2);
+
+para2.addEventListener('dblclick', function () {
+    alert('Et là un double clicque sur le paragraphe!');
+});
+
+// fonctions récursives
+
+function decompte(t) {
+    if ( t > 10 ) {
+        document.getElementById('p3').innerHTML += t + ' - ';
+        return decompte ( t - 10);
+    } else {
+        return t;
+    }
+}
+
+decompte (1000);
