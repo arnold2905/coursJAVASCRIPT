11dce32ee84d40835df1ffda47fce43dcd53affe
Arnold YAYI <87656513+arnold2905@users.noreply.github.com>
2021-11-18 16:42:08 +0100

    cours suite
    cours

--- 00_entrainement/00_exo.html/yayi_exo17.js
+++ 00_entrainement/00_exo.html/yayi_exo17.js
@@ -2,16 +2,19 @@
 
 //alert('coucou');
 
-for (let i = 1; i <= 100; i++) {
-    if(i % 3 == 0 && i % 5 == 0){
-        document.getElementById('p1').innerHTML += '<p style="background: blue; color: #fff;text-align: center;"> GralaBu <br> </p>';
-    }else if(i % 5 == 0){
-        document.getElementById('p1').innerHTML += '<p style="background: orange; color: #fff;text-align: center;"> Bu <br> </p>';
-    }else if(i % 3 == 0){
-        document.getElementById('p1').innerHTML += '<p style="background: lightgreen; color: #fff;text-align: center;"> Grala <br> </p>';
-    }else{
-        document.getElementById('p1').innerHTML += i + '<br>';
-    }
-  }
+// for (let i = 1; i <= 100; i++) {
+//     if(i % 3 == 0 && i % 5 == 0){
+//         document.getElementById('p1').innerHTML += '<p style="background: blue; color: #fff;text-align: center;"> GralaBu <br> </p>';
+//     }else if(i % 5 == 0){
+//         document.getElementById('p1').innerHTML += '<p style="background: orange; color: #fff;text-align: center;"> Bu <br> </p>';
+//     }else if(i % 3 == 0){
+//         document.getElementById('p1').innerHTML += '<p style="background: lightgreen; color: #fff;text-align: center;"> Grala <br> </p>';
+//     }else{
+//         document.getElementById('p1').innerHTML += i + '<br>';
+//     }
+//   }
 
 
+var calculIMC = function(poids, taille) {
+    return poids / (taille * taille);
+    };
